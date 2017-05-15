d5e4ef266465cf2316bc38e8f7b1c680006b3c76
fr <fr58426@gmail.com>
2017-05-15 23:10:02 +0200

    wrote first bits to make sure standard deviation calculation and linear regression are working

--- src/main.d
+++ src/main.d
@@ -1,6 +1,30 @@
 import std.stdio;
 
+import dstats.summary;
+import dstats.regress;
+
 int main(string[] argv)
 {
-    
+    return 1;
+}
+
+unittest
+{
+    //fake data
+    double[] signal = [0.6,1.7,3.2,4.0,4.5,5.8,7.2];
+    //fake time axis
+    double[] time = [1,2,3,4,5,6,7];
+    //calculate data standard deviation
+    double calcSdev = stdev(signal);
+    import std.math;
+    //make sure the right thing is calculated
+    assert(abs(calcSdev - 2.275) < 0.001);
+    //perform a linear regression
+    auto results = linearRegress(signal, repeat(1), time);
+    //check R2
+    assert(abs(results.R2 - 0.986) < 0.001);
+    //check intercept
+    assert(abs(results.betas[0] + 0.328) < 0.001);
+    //check slope
+    assert(abs(results.betas[1] - 1.046) < 0.001);
 }
