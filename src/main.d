c70d27954390d8af85e77bd0af422018b32480c7
fr <fr58426@gmail.com>
2017-05-18 23:06:38 +0200

    Added a class to hold data traces and implemented the functionality for its mean, stdev and slope calcs as buffered properties.

--- src/main.d
+++ src/main.d
@@ -1,9 +1,104 @@
 import std.stdio;
 
-import dstats.summary;
-import dstats.regress;
+class dataTrace
+{
+private:
+    double[] time;
+    double[] data;
+    double mean_;
+    bool meanCalculated = false;
+    double stdev_;
+    bool stdevCalculated = false;
+    double slope_;
+    bool slopeCalculated = false;
+    
+    void calcMean()
+    {
+        import dstats.summary : mean;
+        this.mean_ = mean(this.data);
+    }
+
+    void calcStdev()
+    {
+        import dstats.summary : stdev;
+        this.stdev_ = stdev(data);
+    }
+
+    void calcSlope()
+    {
+        import dstats.regress: linearRegress;
+        import std.range : repeat;
+        //perform a linear regression
+        auto results = linearRegress(this.data, repeat(1), this.time);
+        //get slope from the betas array.
+        this.slope_ = results.betas[1];
+    }
+unittest
+{
+    import dstats.summary : stdev;
+    import dstats.regress;
+    auto testTrace = new dataTrace("a",
+                                   [1,2,3,4,5,6,7],
+                                   [0.6,1.7,3.2,4.0,4.5,5.8,7.2]);
+    import std.math;
+    //make sure the right mean is calculated
+    assert(abs(testTrace.mean - 3.857) < 0.001);
+    //make sure the right stdev is calculated
+    assert(abs(testTrace.stdev - 2.275) < 0.001);
+    //check slope
+    assert(abs(testTrace.slope - 1.046) < 0.001);
+}
+public:
+    string name;
+    this(string name, double[] time, double[] data)
+    in
+    {
+        assert(name !is null);
+        assert(time !is null && time.length > 2);
+        assert(data !is null && data.length > 2);
+        assert(time.length == data.length);
+    }
+    body
+    {
+        this.name = name;
+        this.time = time;
+        this.data = data;
+    }
+
+    double mean() @property
+    {
+        if(!meanCalculated)
+        {
+            calcMean();
+            meanCalculated = true;
+        }
+        return mean_;
+    }
+
+
+    double stdev() @property
+    {
+        if(!stdevCalculated)
+        {
+            calcStdev();
+            stdevCalculated = true;
+        }
+        return stdev_;
+    }
+
+    double slope() @property
+    {
+        if(!slopeCalculated)
+        {
+            calcSlope();
+            slopeCalculated = true;
+        }
+        return slope_;
+    }
+}
 
 string workingDir;
+dataTrace[] datasets;
 
 int main(string[] argv)
 {
@@ -11,38 +106,16 @@ int main(string[] argv)
     if (workingDir.length == 0) {return 1;}
 
     //at this stage, we have found our working directory
+    foreach(f; fileList(workingDir, ["csv"]))
+    {
+        int dataColumn = 3;
+        import std.algorithm.searching : canFind;
+        if(f.canFind("_O2", "_CO2_2"))
+        {
+            dataColumn = 2;
+        }
+    }
     return 0;
-}
-
-unittest
-{
-    //fake data
-    double[] signal = [0.6,1.7,3.2,4.0,4.5,5.8,7.2];
-    //fake time axis
-    double[] time = [1,2,3,4,5,6,7];
-    //calculate data standard deviation
-    double calcSdev = stdev(signal);
-    import std.math;
-    //make sure the right thing is calculated
-    assert(abs(calcSdev - 2.275) < 0.001);
-    //perform a linear regression
-    auto results = linearRegress(signal, repeat(1), time);
-    //check R2
-    assert(abs(results.R2 - 0.986) < 0.001);
-    //check intercept
-    assert(abs(results.betas[0] + 0.328) < 0.001);
-    //check slope
-    assert(abs(results.betas[1] - 1.046) < 0.001);
-
-    //linear regression with intercept fixed at zero
-    results = linearRegress(signal,time);
-    assert(abs(results.betas[0] - 0.980) < 0.001);
-
-    //linear regression with defined intercept
-    double intercept = 0.5;
-    import std.algorithm : map;
-    results = linearRegress(map!(a => a + intercept)(signal), time);
-    assert(abs(results.betas[0] - 1.080) < 0.001);
 }
 
 
